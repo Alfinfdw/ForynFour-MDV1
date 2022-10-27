43c3214ec2e93ce00f159074145b2565641786ed
abilbotz1 <108676663+abilbotz1@users.noreply.github.com>
2022-10-27 21:53:52 +0800

    Update info-tagowner.js

--- plugins/info-tagowner.js
+++ plugins/info-tagowner.js
@@ -2,22 +2,22 @@ let fs = require('fs')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
 let name = m.fromMe ? conn.user : conn.contacts[m.sender]
-let krizyn = 'https://telegra.ph/file/cd37b4f991d633caa6306.jpg'
+let krizyn = 'https://telegra.ph/file/0e4fb9bfb49f985ef053e.jpg'
 await conn.send3ButtonImg(m.chat, krizyn, "=====『 TAG TERDETEKSI 』=====\n\nAda perlu apa panggil panggil bos saya, kangen yaa 🤪", '📮 Silahkan Pilih Button dibawah ini', 'Group', '.allgc', 'Menu', '.menu', 'Store', '.store', m)
                         
 }
-handler.customPrefix = /(@+62 895-3279-34887|@+62895327934887|@62895327934887|@0895327934887)/i
+handler.customPrefix = /(@+62 821-4621-8274|@+6282146218274|@6282146218274|@6282146218274)/i
 handler.command = new RegExp
 
 module.exports = handler
 
 //let handler = async (m, { conn }) => {
-   //let krizyn = `https://telegra.ph/file/cd37b4f991d633caa6306.jpg`
+   //let krizyn = `https://telegra.ph/file/0e4fb9bfb49f985ef053e.jpg`
 //let caption = `Ada perlu apa panggil panggil bos saya, kangen yaa 🤪`
  
 //conn.send3But( m.chat, krizyn, caption, `📮 Silahkan pilih Button dibawah ini`, `Menu`, `.menu`, `Sewa`, `.sewa`, `Group`, `.allgc`, m)
        //}
-//handler.customPrefix = /@krizyn|@krizynofc|krizyn|krizynofc|kri|@kri|@kri aceh/i
+//handler.customPrefix = /@riellofc|@riellofc|riellzyn|riellzynofc|riell|@ri|@ell aceh/i
 //handler.command = new RegExp
 
 //module.exports = handler\\
